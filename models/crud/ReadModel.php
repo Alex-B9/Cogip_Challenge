a4b85e91bfa6ef83d38537da7ecdfbce55633864
Christopher Delecluse <christopherdeleclusepro@gmail.com>
2022-05-12 21:29:05 +0200

    add ReadModel.php

---
+++ models/crud/ReadModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\models\crud;
+
+use App\models\Database;
+
+// TEST PAS SUR QUE CA FONCTIONNE ;D
+
+class ReadModel
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = Database::connect();
+    }
+
+    public function getAllUsers()
+    {
+        $sql = "SELECT * FROM dbname";
+
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute();
+    }
+}
