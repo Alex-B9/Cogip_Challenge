31a488fc263cf324e9f9e74e64efe76c1afcc6c4
Christopher Delecluse <christopherdeleclusepro@gmail.com>
2022-05-12 12:04:56 +0200

    add layout and exemple with home.php

--- views/home.php
+++ views/home.php
@@ -1,1 +1,13 @@
-<h1>Home</h1>
+<?php
+
+$pageTitle = 'Home page';
+ob_get_clean();
+?>
+
+<h1>Home page</h1>
+<p>Hello world !</p>
+
+<?php
+
+$pageContent = ob_get_clean();
+require "layouts/layout.php";
