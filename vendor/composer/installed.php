54d8d285b3303f892c30c9a3b36142fad74c341d
Christopher Delecluse <christopherdeleclusepro@gmail.com>
2022-05-12 16:50:18 +0200

    add models/Database.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5ee3ef640c2f3e6ea57d4febe30af771798d3dd3',
+        'name' => 'diwaa/workshop',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'diwaa/workshop' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5ee3ef640c2f3e6ea57d4febe30af771798d3dd3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
