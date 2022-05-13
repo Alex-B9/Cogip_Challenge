166bc1ee61446e2e14ed2eb5f195cf0f60e91e1f
eliseprts <elisepourtois.pro@gmail.com>
2022-05-13 16:35:23 +0200

    add style to add contact

--- views/contact_new.php
+++ views/contact_new.php
@@ -1,1 +1,49 @@
-<h1>CA MARCHE WOUAT THE FUECK</h1>
+<?php
+$pageCSS = './../public/styles/pages/contactNew/contactNew.css';
+$pageTitle = 'Ajouter contact'; // obligatoire
+ob_start(); // obligatoire
+
+require "views/components/navigation.php";
+
+?>
+    <div class="titleContainer">
+        <h1>Ajout</h1>
+        <h2>Nouveau contact</h2>
+    </div>
+    <div class="addContainer">
+        <div class="addImg"></div>
+        <form class="addForm" action="">
+            <div class="formItem">
+                <label for="lastname">Prénom</label>
+                <input type="text" id="lastname" name="lastname">
+            </div>
+            <div class="formItem">
+                <label for="firstname">Nom</label>
+                <input type="text" id="firstname" name="firstname">
+            </div>
+            <div class="formItem">
+                <label for="phone">Numéro de téléphone</label>
+                <input type="tel" id="phone" name="phone">
+            </div>
+            <div class="formItem">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email">
+            </div>
+            <div class="formItem">
+                <label for="company">Société</label>
+                <select name="company" id="company">
+                    <option>Société 1</option>
+                    <option>Société 2</option>
+                    <option>Société 3</option>
+                </select>
+            </div>
+            <div class="formItem">
+                <button type="submit" value="submit">Ajouter</button>
+            </div>
+        </form>
+    </div>
+
+<?php
+
+$pageContent = ob_get_clean(); // obligatoire
+require "layouts/layout.php"; // obligatoire
