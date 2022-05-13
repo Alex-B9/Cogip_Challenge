e4f3f7eb05fc3c0dafe19791fc508e6156df1045
Anaïs Bossouw <anais.bossouw@gmail.com>
2022-05-13 16:02:12 +0200

    premier jet connect page

--- views/login/connect.php
+++ views/login/connect.php
@@ -1,10 +1,31 @@
 <?php
+$resetCss = "public/styles/reset/reset.css";
+$pageCSS = 'public/styles/pages/connect/connect.css';
 
-$pageTitle = 'Home page';
+$pageTitle = 'Connect page';
 ob_get_clean();
 ?>
+<?php require "views/components/navigation.php"; ?>
+    <h1>Bienvenue A LA COGIP</h1>
+    <break>
+    <p>Bienvenue dans l'espace de connexion de la COGIP</p>
 
-    <h1>Connect</h1>
+    <form method= "post" action="#">
+        <div class="field">
+            <label class="label">Votre identifiant</label>
+            <input class="input" type="text" name="user_email" placeholder="Enter your email" required aria-required=true aria-label="Enter your email">
+        </div>
+
+        <div class="field">
+            <label class="label">Votre mot de passe</label>
+            <input class="input" type="text" name="user_password" placeholder="Enter your password" required aria-required=true aria-label="Enter your password">
+        </div>
+
+        <div class="control">
+            <button type="submit" class="button is-link" aria-required=true aria-label="Submit">SUBMIT</button>
+        </div>
+
+    </form>
 
 <?php
 
