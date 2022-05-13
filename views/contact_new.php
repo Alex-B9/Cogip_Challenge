542bf2e047c0a31029976202154957c5d7c74f07
eliseprts <elisepourtois.pro@gmail.com>
2022-05-13 11:31:19 +0200

    fix problem

--- views/contact_new.php
+++ views/contact_new.php
@@ -1,1 +1,16 @@
 <?php
+$pageCSS = './../public/styles/pages/contactNew.css';
+$pageTitle = 'Ajouter contact'; // obligatoire
+ob_start(); // obligatoire
+
+require "views/components/navigation.php";
+
+?>
+
+    <h1>Bienvenue à la COGIP</h1>
+    <h2>Bonjour !</h2>
+
+<?php
+
+$pageContent = ob_get_clean(); // obligatoire
+require "layouts/layout.php"; // obligatoire
