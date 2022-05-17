4b24fab757c2a63c048e54de41568ab63f2c5f6f
eliseprts <elisepourtois.pro@gmail.com>
2022-05-17 11:11:08 +0200

    add html to contact_details.php

--- views/contact_details.php
+++ views/contact_details.php
@@ -8,7 +8,34 @@ require "views/components/navigation.php";
 
 ?>
 
-<h1>Contact details</h1>
+<div class="titleContainer">
+    <h1>Contact</h1>
+    <h2 id="contactName">Nom du contact</h2>
+</div>
+<div class="detailsContainer">
+    <div class="contactInfo">
+        <p>Société : <span id="contactCompany">nom de la société</span></p>
+        <p>Email : <span id="contactEmail">email du contact</span></p>
+        <p>Numéro de téléphone : <span id="contactPhone">numéro de téléphone du contact</span></p>
+    </div>
+    <div class="contactInvoices">
+        <h3>Factures liées au contact</h3>
+        <table class="contactInvoicesTable">
+            <tr>
+                <th>Numéro de facture</th>
+                <th>Date</th>
+            </tr>
+            <tr>
+                <td id="contactInvoiceNbr">123456789</td>
+                <td id="contactInvoiceDate">17/05/2022</td>
+            </tr>
+            <tr>
+                <td id="contactInvoiceNbr">123456789</td>
+                <td id="contactInvoiceDate">18/05/2022</td>
+            </tr>
+        </table>
+    </div>
+</div>
 
 <?php
 
