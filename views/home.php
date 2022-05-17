1aaf6123ac7e2e4d859617e3d535a4768f17864a
Amoryne <amoryne.jolimont@gmail.com>
2022-05-17 16:26:35 +0200

    j'écris un message pour dire que je fais la page home

--- views/home.php
+++ views/home.php
@@ -10,6 +10,48 @@ require "views/components/navigation.php";
     <h1>Bienvenue à la COGIP</h1>
     <h2>Bonjour !</h2>
 
+    <p>Dernières factures</p>
+    <table class="table">
+        <thead>
+            <th>Numéro facture</th>
+            <th>Date</th>
+            <th>Société</th>
+            <th></th>
+             </thead>
+        <tbody>
+           <tr>
+               <td class="facture-style">OFKZSODJOS1</td>
+               <td>00/00/01</td>
+               <td>societé1</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+           <tr>
+               <td class="facture-style">OFKZSODJOS2</td>
+               <td>00/00/02</td>
+               <td>societé2</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+           <tr>
+               <td class="facture-style">OFKZSODJOS3</td>
+               <td>00/00/03</td>
+               <td>societé3</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+           <tr>
+               <td class="facture-style">OFKZSODJOS4</td>
+               <td>00/00/04</td>
+               <td>societé4</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+           <tr>
+               <td class="facture-style">OFKZSODJOS5</td>
+               <td>00/00/05</td>
+               <td>societé5</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+        </tbody>
+    </table>
+
 <?php
 
 $pageContent = ob_get_clean(); // obligatoire
