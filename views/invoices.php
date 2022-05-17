d34f2c49edc4d64c6b646d99e4ae1fa95d3b760e
Anaïs Bossouw <anais.bossouw@gmail.com>
2022-05-17 11:54:43 +0200

    start

--- views/invoices.php
+++ views/invoices.php
@@ -1,1 +1,10 @@
-<h1>Invoices</h1>
+<?php
+
+$resetCss = "public/styles/reset/reset.css";
+$pageCSS = 'public/styles/pages/connect/list.css';
+$pageTitle = 'Factures';
+ob_start();
+?>
+
+<?php require "views/components/navigation.php"; ?>
+
