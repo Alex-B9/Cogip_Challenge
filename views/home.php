e4287d13cdda7c58277cdea322d0e416f625a3f3
Amoryne <amoryne.jolimont@gmail.com>
2022-05-19 10:34:34 +0200

    home

--- views/home.php
+++ views/home.php
@@ -8,11 +8,100 @@ require "views/components/navigation.php";
 
 ?>
 
-
+<div class='container'>
     <h1>Bienvenue à la COGIP</h1>
-    <h2>Bonjour !</h2>
-
-    <p>Dernières factures</p>
+    <h2>Bonjour Jean-Christian !
+Que souhaitez-vous faire aujourd’hui ?</h2>
+<div class='container-table'>
+    <div class="all-table">
+    <div class="titre-table">
+        <p >Dernières factures :</p>
+    </div>
+    <div>
+    <table class="table">
+        <thead>
+            <th>Numéro facture</th>
+            <th>Date</th>
+            <th>Société</th>
+            <th></th>
+             </thead>
+        <tbody>
+           <tr>
+               <td class="facture-style">OFKZSODJOS1</td>
+               <td>00/00/01</td>
+               <td>societé1</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+           <tr>
+               <td class="facture-style">OFKZSODJOS2</td>
+               <td>00/00/02</td>
+               <td>societé2</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+           <tr>
+               <td class="facture-style">OFKZSODJOS3</td>
+               <td>00/00/03</td>
+               <td>societé3</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+           <tr>
+               <td class="facture-style">OFKZSODJOS4</td>
+               <td>00/00/04</td>
+               <td>societé4</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+           <tr>
+               <td class="facture-style">OFKZSODJOS5</td>
+               <td>00/00/05</td>
+               <td>societé5</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+        </tbody>
+    </table></div>
+    </div>
+    <div class='container-table'>
+    <p class="titre-table">Dernières société :</p>
+    <table class="table">
+        <thead>
+            <th>Numéro facture</th>
+            <th>Date</th>
+            <th>Société</th>
+            <th></th>
+             </thead>
+        <tbody>
+           <tr>
+               <td class="facture-style">OFKZSODJOS1</td>
+               <td>00/00/01</td>
+               <td>societé1</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+           <tr>
+               <td class="facture-style">OFKZSODJOS2</td>
+               <td>00/00/02</td>
+               <td>societé2</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+           <tr>
+               <td class="facture-style">OFKZSODJOS3</td>
+               <td>00/00/03</td>
+               <td>societé3</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+           <tr>
+               <td class="facture-style">OFKZSODJOS4</td>
+               <td>00/00/04</td>
+               <td>societé4</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+           <tr>
+               <td class="facture-style">OFKZSODJOS5</td>
+               <td>00/00/05</td>
+               <td>societé5</td>
+               <td><img src="../public/assets/img/delete-2.png" alt="delete"></td>
+           </tr>
+        </tbody>
+    </table></div>
+    <p class="titre-table">Dernier contact :</p>
     <table class="table">
         <thead>
             <th>Numéro facture</th>
@@ -53,7 +142,8 @@ require "views/components/navigation.php";
            </tr>
         </tbody>
     </table>
-
+</div>
+</div>
 <?php
 
 $pageContent = ob_get_clean(); // obligatoire
