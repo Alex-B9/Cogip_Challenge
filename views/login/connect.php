0aafafb9fa84c2fdb9286473434e4ce1b1ecee82
Anaïs Bossouw <anais.bossouw@gmail.com>
2022-05-19 10:06:52 +0200

    rajout background et légères modifs

--- views/login/connect.php
+++ views/login/connect.php
@@ -7,34 +7,36 @@ ob_start();
 ?>
 
 <?php require "views/components/navigation.php"; ?>
+    <div class="titleContainer">
+        <h1>Bienvenue</h1>
+        <h2>A la Cogip</h2>
+        <h3>Bienvenue dans l'espace de connexion de la COGIP</h3>
+    </div>
 
-    <h1>BIENVENUE A LA COGIP</h1>
-    <break>
-        <p>Bienvenue dans l'espace de connexion de la COGIP</p>
-
-        <div class="connect-container">
+    <div class="connect-container">
             <div class="img">
                 <img src="./public/assets/img/data-security-1.png" alt="logo-security">
-                <div>
+            </div>
 
-                    <form class="form" method= "post" action="../../core/loginForm.php">
-                        <div class="field">
-                            <label class="label">Votre identifiant</label>
-                            <input class="input" type="text" id="userEmail" name="userEmail" placeholder="Enter your email" required aria-required=true aria-label="Enter your email">
-                        </div>
+        
+                <form class="form" method= "post" action="../../core/loginForm.php">
+                    <div class="field">
+                        <label class="label">Votre identifiant</label>
+                        <input class="input" type="text" id="userEmail" name="userEmail" placeholder="Entrez votre email" required aria-required=true aria-label="Enter your email">
+                    </div>
 
-                        <div class="field">
-                            <label class="label">Votre mot de passe</label>
-                            <input class="input" type="text" id="userPassword" name="userPassword" placeholder="Enter your password" required aria-required=true aria-label="Enter your password">
-                        </div>
+                    <div class="field">
+                        <label class="label">Votre mot de passe</label>
+                        <input class="input" type="text" id="userPassword" name="userPassword" placeholder="Entrez votre mot de passe" required aria-required=true aria-label="Enter your password">
+                    </div>
 
-                        <a href="register">Pas encore inscrit ? Cliquez ici</a>
+                    <a href="register">Pas encore inscrit ? Cliquez ici</a>
 
-                        <div class="control">
-                            <button type="submit" name="Submit" aria-required=true aria-label="Submit">SUBMIT</button>
-                        </div>
-                    </form>
-                </div>
+                    <div class="control">
+                        <button type="submit" name="Submit" aria-required=true aria-label="Submit">SUBMIT</button>
+                    </div>
+                </form>
+            
 
 <?php
 
