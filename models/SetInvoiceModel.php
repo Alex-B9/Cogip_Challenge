cee5bfdc9c7238f8665bb25147e70b2d2fc44350
Alex-B9 <98591644+Alex-B9@users.noreply.github.com>
2022-05-23 13:43:19 +0200

    Update fixbug

--- models/SetInvoiceModel.php
+++ models/SetInvoiceModel.php
@@ -7,8 +7,6 @@ use App\models\crud\CreateModel;
 class SetInvoiceModel
 {
     private int $invoiceNumber;
-//    private int $lastname;
-//    private string $email;
     private int $date;
     private string $company;
     private string $contact;
@@ -18,16 +16,6 @@ class SetInvoiceModel
     public function setInvoiceNumber(int $invoiceNumber): void
     {
         $this->invoiceNumber = $invoiceNumber;
-    }
-
-    public function setLastname(string $lastname): void
-    {
-        $this->lastname = $lastname;
-    }
-
-    public function setEmail(string $email): void
-    {
-        $this->email = $email;
     }
 
     public function setDate(int $date): void
