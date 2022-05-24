d74085e969f85e8b84e578f5fccc606fc82fad2f
eliseprts <elisepourtois.pro@gmail.com>
2022-05-24 10:53:14 +0200

    add dropdown menu for admin

--- views/components/navigation.php
+++ views/components/navigation.php
@@ -7,12 +7,25 @@
         </a>
         </div>
         <div class='linksContainer'>
-            <ul>
-                <li><a href='/'>Accueil</a></li>
-                <li><a href='/invoices'>Factures</a></li>
-                <li><a href='/companies'>Sociétés</a></li>
-                <li class="connect-btn"><a href='/contacts'>Contacts</a></li>
-                <li><a href='/connect'>Connexion</a></li>
+            <ul class='mainMenu'>
+                <li class='mainMenuItem'><a href='/'>Accueil</a></li>
+                <li class='mainMenuItem'><a href='/invoices'>Factures</a></li>
+                <li class='mainMenuItem'><a href='/companies'>Sociétés</a></li>
+                <li class='mainMenuItem'><a href='/contacts'>Contacts</a></li>
+
+                <?php if(isset($_SESSION['Username'])): ?>
+                    <li class='adminMenu'><a href='#'><?php echo 'Admin'; ?></a>
+                        <div class='adminMenuContent'>
+                                <a href="/dashboard"><?php echo 'Dashboard'; ?></a>
+                                <a href="/invoice-new"><?php echo 'Ajouter une facture'; ?></a>
+                                <a href="/company-new"><?php echo 'Ajouter une société'; ?></a>
+                                <a href="/contact-new"><?php echo 'Ajouter un contact'; ?></a>
+                        </div>
+                    </li>
+                <?php else:?>
+                    <li class='connexionItem'><a href='/connect'><?php echo 'Connexion'; ?></a></li>
+                <?php endif;?>
+
             </ul>
         </div>
     </nav>
